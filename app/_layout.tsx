08b8308cc5e4aa093d231fe160d043807dbfa9dd
Juan Ceballo <edu17290@gmail.com>
2025-04-09 16:19:23 -0300

    created content to movie details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,17 @@
 import { Stack } from "expo-router";
 import './globals.css';
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
-  </Stack>;
+  return (
+    <>
+      {/* Oculta la barra que muestra hora y bateria */}
+      <StatusBar hidden={true}/>
+
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+      </Stack>;
+    </>
+  )
 }
